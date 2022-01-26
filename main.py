6419e7e306a08c6a2297084608e2006dc4801aa4
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-01-27 00:39:25 +0300

    Add new functions in main logic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import xkcd_utilities
 import vk_utilities
 import os
 from dotenv import load_dotenv
+import random
 
 
 def main():
@@ -9,14 +10,17 @@ def main():
     vk_access_token = os.getenv('vk_access_token')
     vk_api_version = 5.131
     vk_group_id = os.getenv('vk_group_id')
-    comics_meta = xkcd_utilities.get_xckd_comics_meta(353)
-    xkcd_utilities.save_xckd_pic_file(comics_meta)
+    latest_comics = xkcd_utilities.get_latest_comics_number()
+    comics_meta = xkcd_utilities.get_xckd_comics_meta(
+        random.randint(1, latest_comics)
+    )
+    comics_filepath = xkcd_utilities.save_xckd_pic_file(comics_meta)
     comics_comment = xkcd_utilities.get_author_comment(comics_meta)
     upload_data = vk_utilities.get_upload_url(
         vk_access_token, vk_api_version, vk_group_id
     )
     uploaded_photo = vk_utilities.upload_picture_to_server(
-        upload_data['response']['upload_url'], 'Python.png')
+        upload_data['response']['upload_url'], comics_filepath)
     saved_photo = vk_utilities.save_photo_to_album(
         vk_group_id, uploaded_photo['photo'], uploaded_photo['server'],
         uploaded_photo['hash'], vk_access_token, vk_api_version
@@ -25,6 +29,8 @@ def main():
         f'-{vk_group_id}', 1, comics_comment, saved_photo['id'],
         saved_photo['owner_id'], vk_access_token, vk_api_version
     )
+    vk_utilities.publish_photo()
+    os.remove(comics_filepath)
 
 
 if __name__ == '__main__':

c8185f6d951bd1ed9c2a637feb2090184e8a6703
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-01-27 23:07:47 +0300

    Fix naming

--- main.py
+++ main.py
@@ -10,26 +10,27 @@ def main():
     vk_access_token = os.getenv('vk_access_token')
     vk_api_version = 5.131
     vk_group_id = os.getenv('vk_group_id')
+    post_sender = {'user': 0, 'group': 1}
     latest_comics = xkcd_utilities.get_latest_comics_number()
-    comics_meta = xkcd_utilities.get_xckd_comics_meta(
+    comics = xkcd_utilities.get_xckd_comics(
         random.randint(1, latest_comics)
     )
-    comics_filepath = xkcd_utilities.save_xckd_pic_file(comics_meta)
-    comics_comment = xkcd_utilities.get_author_comment(comics_meta)
-    upload_data = vk_utilities.get_upload_url(
+    comics_filepath = xkcd_utilities.save_xckd_pic_file(comics)
+    comics_comment = xkcd_utilities.get_author_comment(comics)
+    upload_url = vk_utilities.get_upload_url(
         vk_access_token, vk_api_version, vk_group_id
     )
     uploaded_photo = vk_utilities.upload_picture_to_server(
-        upload_data['response']['upload_url'], comics_filepath)
+        upload_url['response']['upload_url'], comics_filepath)
     saved_photo = vk_utilities.save_photo_to_album(
         vk_group_id, uploaded_photo['photo'], uploaded_photo['server'],
         uploaded_photo['hash'], vk_access_token, vk_api_version
     )
     vk_utilities.publish_photo(
-        f'-{vk_group_id}', 1, comics_comment, saved_photo['id'],
-        saved_photo['owner_id'], vk_access_token, vk_api_version
+        f'-{vk_group_id}', post_sender['group'], comics_comment,
+        saved_photo['id'], saved_photo['owner_id'], vk_access_token,
+        vk_api_version
     )
-    vk_utilities.publish_photo()
     os.remove(comics_filepath)
 
 

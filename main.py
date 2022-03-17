3e153e0195e67525375cef208b4723d52941bb59
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-03-17 23:23:23 +0300

    Shorten try except block

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ import random
 
 
 def main():
+    load_dotenv()
+    vk_access_token = os.getenv('VK_ACCESS_TOKEN')
+    vk_api_version = 5.131
+    vk_group_id = os.getenv('VK_GROUP_ID')
+    post_sender = {'user': 0, 'group': 1}
+    latest_comics = xkcd_tools.get_latest_comics_number()
+    comics = xkcd_tools.get_xckd_comics(
+        random.randint(1, latest_comics)
+    )
+    comics_filepath = xkcd_tools.save_xckd_pic_file(comics)
+    comics_comment = comics['alt']
     try:
-        load_dotenv()
-        vk_access_token = os.getenv('VK_ACCESS_TOKEN')
-        vk_api_version = 5.131
-        vk_group_id = os.getenv('VK_GROUP_ID')
-        post_sender = {'user': 0, 'group': 1}
-        latest_comics = xkcd_tools.get_latest_comics_number()
-        comics = xkcd_tools.get_xckd_comics(
-            random.randint(1, latest_comics)
-        )
-        comics_filepath = xkcd_tools.save_xckd_pic_file(comics)
-        comics_comment = comics['alt']
         upload_url = vk_api_tools.get_upload_url(
             vk_access_token, vk_api_version, vk_group_id
         )
